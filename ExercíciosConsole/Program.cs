e76a9739e1655078840c178aaff6fac0174b47d9
Usuario <Usuario@CPD03-M19>
2024-03-26 10:39:26 -0300

    Adicionar arquivos de projeto.

---
+++ ExercíciosConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercíciosConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+
+            num = 0;
+
+            while (num <= 100) 
+            {
+                Console.WriteLine(num);
+
+                num += 2;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
